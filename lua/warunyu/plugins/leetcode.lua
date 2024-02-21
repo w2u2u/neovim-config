f41bd789014db35277f1c9fe8c0c923ed13d83a3
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-21 15:01:13 +0700

    Fix leetcode setup

--- lua/warunyu/plugins/leetcode.lua
+++ lua/warunyu/plugins/leetcode.lua
@@ -1,10 +1,26 @@
 return {
-	-- configuration goes here
-	arg = "leetcode.nvim",
-	lang = "rust",
-	injector = {
-		["golang"] = {
-			before = { "package main", "" },
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim", -- required by telescope
+		"MunifTanjim/nui.nvim",
+
+		-- optional
+		"nvim-treesitter/nvim-treesitter",
+		"rcarriga/nvim-notify",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = "leetcode.nvim" ~= vim.fn.argv()[1],
+	opts = {
+
+		-- configuration goes here
+		arg = "leetcode.nvim",
+		lang = "rust",
+		injector = {
+			["golang"] = {
+				before = { "package main", "" },
+			},
 		},
 	},
 }
