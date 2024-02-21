30648e5a02288d2c580c7e1d2ac2e64e7a96cf3e
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-21 22:19:13 +0700

    Custom lualine look

--- lua/warunyu/plugins/lualine.lua
+++ lua/warunyu/plugins/lualine.lua
@@ -8,6 +8,30 @@ return {
 			options = {
 				theme = vim.g.colors_name,
 				globalstatus = true,
+				section_separators = {
+					left = "",
+					right = "",
+				},
+				component_separators = {
+					left = "",
+					right = "",
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "filetype", "filename" },
+				lualine_c = { "branch", "diff", "diagnostics" },
+				lualine_x = { "encoding" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
