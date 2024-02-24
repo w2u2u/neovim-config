2df4eec7f5640890521e07578ef8c94e6c09cfd0
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-25 02:23:31 +0700

    Change some default keymaps

--- lua/warunyu/init.lua
+++ lua/warunyu/init.lua
@@ -8,5 +8,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
 
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("n", "<ESC>", ":noh<CR>")
+vim.keymap.set("i", "jk", "<ESC>") -- Exit insert mode
+vim.keymap.set("n", "<ESC>", ":noh<CR>") -- Clear search highlights
+vim.keymap.set("n", "<C-Tab>", "<C-w>w") -- Switch between windows
