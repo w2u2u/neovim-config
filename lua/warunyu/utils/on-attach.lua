29614d3f6a2be935b9c08bc9981873de44c8d018
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-25 02:23:13 +0700

    Change diagnostic movement with j/k

--- lua/warunyu/utils/on-attach.lua
+++ lua/warunyu/utils/on-attach.lua
@@ -45,10 +45,10 @@ local on_attach = function(_, bufnr)
 	keymap.set("n", "<leader>df", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 	opts.desc = "Go to previous diagnostic"
-	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+	keymap.set("n", "<leader>dj", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
 	opts.desc = "Go to next diagnostic"
-	keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+	keymap.set("n", "<leader>dk", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
 	opts.desc = "Show documentation for what is under cursor"
 	keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
