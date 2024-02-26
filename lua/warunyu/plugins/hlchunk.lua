1986eca838575ef82606648f8448f65d4182ff38
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-26 12:41:09 +0700

    Config style for hlchunk

--- lua/warunyu/plugins/hlchunk.lua
+++ lua/warunyu/plugins/hlchunk.lua
@@ -2,13 +2,25 @@ return {
 	"shellRaining/hlchunk.nvim",
 	event = { "UIEnter" },
 	config = function()
+		local colors_name = vim.g.colors_name
+		local color = "#c6a0f6"
+
+		if colors_name == "gruvbox" then
+			color = "#fb4934"
+		end
+
 		require("hlchunk").setup({
+			chunk = {
+				enable = true,
+				style = { color },
+			},
 			indent = {
 				enable = false,
 				use_treesitter = true,
 			},
 			line_num = {
 				use_treesitter = true,
+				style = { color },
 			},
 			blank = {
 				enable = false,
